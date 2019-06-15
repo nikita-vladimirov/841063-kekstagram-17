604a38dc5b7d42c65a31207faf92d0e7d99dc7dc
nikita-vladimirov <vladimirov46@gmail.com>
2019-06-15 19:05:46 +0300

    Исправляет стилевые ошибки в коде

--- js/main.js
+++ js/main.js
@@ -1,13 +1,6 @@
 var publishedPosts = [];
-var numberPosts = 25; //Количество необходимых постов
-var comments = [
-                'Всё отлично!',
-                'В целом всё неплохо. Но не всё.',
-                'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-                'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-                'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-                'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
-              ];
+var numberPosts = 25; // Количество необходимых постов
+var comments = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
 
 function getRandomValue(min, max) {
   var randomValue = min + Math.random() * (max + 1 - min);
@@ -15,19 +8,14 @@ function getRandomValue(min, max) {
   return randomValue;
 }
 
-function createPosts () {
+function createPosts() {
   for (var i = 0; i < numberPosts; i++) {
     var url = 'photos/' + Number(i + 1) + '.jpg';
     var likes = getRandomValue(15, 200);
     var message = comments[getRandomValue(0, (comments.length - 1))];
 
-    publishedPosts[i] = {
-                         avatar: url,
-                         likes: likes,
-                         message: message
-                        };
+    publishedPosts[i] = {avatar: url, likes: likes, message: message};
   }
-  console.log(publishedPosts);
 }
 
 createPosts();
