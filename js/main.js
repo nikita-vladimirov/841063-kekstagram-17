59993bc23fd4d9f2e113488b3af936888786a689
nikita-vladimirov <vladimirov46@gmail.com>
2019-06-19 20:22:10 +0300

    Добавляет функцию публикации постов на главной странице

--- js/main.js
+++ js/main.js
@@ -2,7 +2,16 @@
 
 var publishedPosts = [];
 var numberPosts = 25; // Количество необходимых постов
-var comments = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+var comments = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+var postTemlate = document.querySelector('#picture').content.querySelector('.picture');
+var fragment = document.createDocumentFragment();
 
 function getRandomValue(min, max) {
   var randomValue = min + Math.random() * (max + 1 - min);
@@ -20,18 +29,18 @@ function createPosts() {
   }
 }
 
-createPosts();
+function publishPosts() {
+  for (var i = 0; i < numberPosts; i++) {
+    var post = postTemlate.cloneNode(true);
 
-var postTemlate = document.querySelector('#picture').content.querySelector('.picture');
-var fragment = document.createDocumentFragment();
+    post.querySelector('img').src = publishedPosts[i].avatar;
+    post.querySelector('.picture__likes').textContent = publishedPosts[i].likes;
+    post.querySelector('.picture__comments').textContent = publishedPosts[i].message;
 
-for (var i = 0; i < numberPosts; i++) {
-  var post = postTemlate.cloneNode(true);
-
-  post.querySelector('img').src = publishedPosts[i].avatar;
-  post.querySelector('.picture__likes').textContent = publishedPosts[i].likes;
-  post.querySelector('.picture__comments').textContent = publishedPosts[i].message;
-
-  fragment.appendChild(post);
-  document.querySelector('.pictures').appendChild(fragment);
+    fragment.appendChild(post);
+    document.querySelector('.pictures').appendChild(fragment);
+  }
 }
+
+createPosts();
+publishPosts();
